9c020cfedd936efb0d3650f41f6c0f36aec714a5
dangersflow <pacog879@hotmail.com>
2021-05-23 00:59:53 -0500

    first commit

---
+++ lib/Home.dart
@@ -0,0 +1,101 @@
+import 'package:flutter/material.dart';
+import 'package:google_nav_bar/google_nav_bar.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:animate_do/animate_do.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+String fbUrl = "https://www.facebook.com/profile.php?id=100016416902538";
+String liUrl = "https://www.linkedin.com/in/francisco-gonzalez-861333134";
+String ghUrl = "https://github.com/dangersflow";
+String twUrl = "https://twitter.com/Frank10346677";
+String dvUrl = "https://dev.to/dangersflow";
+String asUrl = "https://asarg.hackresearch.com/main/";
+
+class HomePage extends StatefulWidget {
+  HomePage({Key? key, required bool animate}) : super(key: key);
+
+  bool animate = true;
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  bool _animate = false;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _animate = widget.animate;
+    print(_animate);
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child:SingleChildScrollView(
+        child:  Row(
+          children: [
+            Column(
+              children: [
+                Padding(padding: EdgeInsets.fromLTRB(0, MediaQuery.of(context).size.height/10, 0, 0)),
+                FadeInDown(
+                  child: Container(
+                    child: Text("I'm a Software Dev :)", style: GoogleFonts.catamaran(fontSize: 48, fontWeight: FontWeight.bold, color: Colors.white),),
+                  ),
+                  delay: Duration(milliseconds: 600),
+                ),
+                Padding(padding: EdgeInsets.fromLTRB(0, MediaQuery.of(context).size.height/3, 0, 0)),
+                BounceInDown(child: Container(child: IconButton(onPressed: () async => await canLaunch(fbUrl) ? await launch(fbUrl) : throw 'Could not launch $fbUrl', icon: Icon(FontAwesomeIcons.facebook), iconSize: 64), padding: EdgeInsets.fromLTRB(0, 0, MediaQuery.of(context).size.width/4, 0),), delay: Duration(milliseconds: 1000), animate: _animate,),
+                Padding(padding: EdgeInsets.fromLTRB(0, 40, 0, 0)),
+                BounceInDown(child: Container(child: IconButton(onPressed: () async => await canLaunch(liUrl) ? await launch(liUrl) : throw 'Could not launch $liUrl', icon: Icon(FontAwesomeIcons.linkedin), iconSize: 64)), delay: Duration(milliseconds: 1100),),
+                Padding(padding: EdgeInsets.fromLTRB(0, 40, 0, 0)),
+                BounceInDown(child: Container(child: IconButton(onPressed: () async => await canLaunch(ghUrl) ? await launch(ghUrl) : throw 'Could not launch $ghUrl', icon: Icon(FontAwesomeIcons.github), iconSize: 64), padding: EdgeInsets.fromLTRB(MediaQuery.of(context).size.width/4, 0, 0, 0),), delay: Duration(milliseconds: 1200),)
+              ],
+            ),
+            Column(
+              children: [
+                FadeInDown(
+                  child: Container(
+                    child: Text("Hello! My name is Frank.", style: GoogleFonts.catamaran(fontSize: 48, fontWeight: FontWeight.bold, color: Colors.white),),
+                  ),
+                  delay: Duration(milliseconds: 400),
+                ),
+                FadeInDown(
+                  child: Container(
+                    child: Text("Check out my socials and/or repos!", style: GoogleFonts.catamaran(fontSize: 48, fontWeight: FontWeight.bold, color: Colors.white),),
+                  ),
+                  delay: Duration(milliseconds: 600),
+                ),
+                FadeInDown(
+                  child: Container(
+                    child: Image.asset("graphics/me.png", scale: 4,),
+                  ),
+                  delay: Duration(milliseconds: 800),
+                ),
+              ],
+            ),
+            Column(
+              children: [
+                Padding(padding: EdgeInsets.fromLTRB(0, MediaQuery.of(context).size.height/10, 0, 0)),
+                FadeInDown(
+                  child: Container(
+                    child: Text("I like games and music too! :D", style: GoogleFonts.catamaran(fontSize: 48, fontWeight: FontWeight.bold, color: Colors.white),),
+                  ),
+                  delay: Duration(milliseconds: 1000),
+                ),
+                Padding(padding: EdgeInsets.fromLTRB(0, MediaQuery.of(context).size.height/3, 0, 0)),
+                BounceInDown(child: Container(child: IconButton(onPressed: () async => await canLaunch(asUrl) ? await launch(asUrl) : throw 'Could not launch $asUrl', icon: Icon(FontAwesomeIcons.university), iconSize: 64), padding: EdgeInsets.fromLTRB(MediaQuery.of(context).size.width/4, 0, 0, 0),), delay: Duration(milliseconds: 1500),),
+                Padding(padding: EdgeInsets.fromLTRB(0, 40, 0, 0)),
+                BounceInDown(child: Container(child: IconButton(onPressed: () async => await canLaunch(dvUrl) ? await launch(dvUrl) : throw 'Could not launch $dvUrl', icon: Icon(FontAwesomeIcons.dev), iconSize: 64)), delay: Duration(milliseconds: 1400),),
+                Padding(padding: EdgeInsets.fromLTRB(0, 40, 0, 0)),
+                BounceInDown(child: Container(child: IconButton(onPressed: () async => await canLaunch(twUrl) ? await launch(twUrl) : throw 'Could not launch $twUrl', icon: Icon(FontAwesomeIcons.twitter), iconSize: 64,), padding: EdgeInsets.fromLTRB(0, 0, MediaQuery.of(context).size.width/4, 0),), delay: Duration(milliseconds: 1300),)
+              ],
+            )
+          ],
+          mainAxisAlignment: MainAxisAlignment.center,
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,175 @@
+import 'package:animate_do/animate_do.dart';
+import 'package:flutter/material.dart';
+import 'package:google_nav_bar/google_nav_bar.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'Home.dart';
+
+bool animatePageOne = true;
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+
+      ),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  PageController _pageController = PageController();
+  int _selectedIndex = 0;
+  static const TextStyle optionStyle =
+  TextStyle(fontSize: 30, fontWeight: FontWeight.w600);
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    _pageController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    //
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
+    return AspectRatio(aspectRatio: 16/9,
+      child: Scaffold(
+
+        backgroundColor: Color(0xFF1B263B),
+        body: Stack(
+          children: [
+            RotatedBox(quarterTurns: 2,
+              child: Container(
+                decoration: BoxDecoration(
+                    image: DecorationImage(image: AssetImage("graphics/gradient.png"), fit: BoxFit.cover)
+                ),
+              ),
+            ),
+            PageView(
+              controller: _pageController,
+              physics: NeverScrollableScrollPhysics(),
+              children: <Widget>[
+                HomePage(animate: animatePageOne,),
+                Text(
+                  'Likes',
+                  style: optionStyle,
+                ),
+                Text(
+                  'Search',
+                  style: optionStyle,
+                ),
+                Text(
+                  'Profile',
+                  style: optionStyle,
+                ),
+              ],
+            )
+          ],
+        ),
+        bottomNavigationBar: Container(
+          decoration: BoxDecoration(
+            color: Colors.white,
+            boxShadow: [
+              BoxShadow(
+                blurRadius: 20,
+                color: Colors.black.withOpacity(.1),
+              )
+            ],
+          ),
+          child: SafeArea(
+            child: Padding(
+              padding: EdgeInsets.symmetric(horizontal: MediaQuery.of(context).size.width/4, vertical: 8),
+              child: GNav(
+                rippleColor: Colors.grey[300]!,
+                hoverColor: Colors.grey[100]!,
+                gap: 8,
+                activeColor: Colors.black,
+                iconSize: 24,
+                padding: EdgeInsets.symmetric(horizontal: 20, vertical: 12),
+                duration: Duration(milliseconds: 400),
+                tabBackgroundColor: Colors.grey[100]!,
+                color: Colors.black,
+                tabs: [
+                  GButton(
+                    icon: FontAwesomeIcons.home,
+                    text: 'Home',
+                  ),
+                  GButton(
+                    icon: FontAwesomeIcons.heart,
+                    text: 'Likes',
+                  ),
+                  GButton(
+                    icon: FontAwesomeIcons.search,
+                    text: 'Search',
+                  ),
+                  GButton(
+                    icon: FontAwesomeIcons.user,
+                    text: 'Profile',
+                  ),
+                ],
+                selectedIndex: _selectedIndex,
+                onTabChange: (index) {
+                  setState(() {
+                    _selectedIndex = index;
+                    //
+                    //
+                    //using this page controller you can make beautiful animation effects
+                    _pageController.animateToPage(index,
+                        duration: Duration(milliseconds: 500), curve: Curves.easeOutSine);
+                    animatePageOne = false;
+                  });
+                },
+              ),
+            ),
+          ),
+        ),
+      )
+    );
+  }
+}
